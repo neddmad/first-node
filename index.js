1f061a1960f6dc243c51d11a980ee00e72f58536
Balaban <neddmad1@gmail.com>
2023-07-21 19:16:04 +0300

    Add text from first.txt to second.txt

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ fs.appendFile('second.txt', 'I like NodeJS', function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
+fs.writeFile('second.txt', '\nЯ люблю NodeJS', err => {
+    if (err) {
+      console.error(err);
+    }
+});
